b99d0b0c2401c51ea69f610d9d33a2221b3210e7
Efe G <efeasiughu@gmail.com>
2024-02-18 09:48:37 -0500

    working on some component creation

--- pages/project/erc721.tsx
+++ pages/project/erc721.tsx
@@ -1,5 +1,8 @@
+import styles from "../../styles/Home.module.css";
+
 const ERC721ProjectPage = () => {
-  return <div>ERC721ProjectPage</div>;
+
+  return <div className={styles.container}>ERC721ProjectPage</div>;
 };
 
 export default ERC721ProjectPage;
