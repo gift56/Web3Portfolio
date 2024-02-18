04284aa21fe65c13d5b3ffe7ab1e9a9c91403768
Efe G <efeasiughu@gmail.com>
2024-02-18 17:52:19 -0500

    integrated most of my smart contract

--- pages/project/profilestatus.tsx
+++ pages/project/profilestatus.tsx
@@ -1,11 +1,20 @@
 import styles from "../../styles/Home.module.css";
 import { HeroCard } from "../../components";
 import { PROFILE_STATUS_CONTRACT_ADDRESS } from "../../constants/addresses";
-import { useContract, useContractMetadata } from "@thirdweb-dev/react";
+import {
+  useAddress,
+  useContract,
+  useContractMetadata,
+  useContractRead,
+} from "@thirdweb-dev/react";
 
 const ProfileStatusPage = () => {
+  const address = useAddress();
   const { contract } = useContract(PROFILE_STATUS_CONTRACT_ADDRESS);
   const { data: contractMetadata, isLoading } = useContractMetadata(contract);
+
+  const { data: currentStatus, isLoading: isLoadingCurrentStatus } =
+    useContractRead(contract, "getStatus", [address]);
 
   return (
     <div className={styles.container}>
