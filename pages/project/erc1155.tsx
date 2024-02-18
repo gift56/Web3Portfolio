5b2bf5759613812444b89d056dfb68ff85c52010
Efe G <efeasiughu@gmail.com>
2024-02-18 16:14:04 -0500

    working on some component creation

--- pages/project/erc1155.tsx
+++ pages/project/erc1155.tsx
@@ -1,12 +1,23 @@
 import { useContract, useContractMetadata } from "@thirdweb-dev/react";
 import React from "react";
 import { ERC1155_CONTRACT_ADDRESS } from "../../constants/addresses";
+import { HeroCard } from "../../components";
+import styles from "../../styles/Home.module.css";
 
 const ERC1155ProjectPage = () => {
   const { contract } = useContract(ERC1155_CONTRACT_ADDRESS, "edition-drop");
   const { data: contractMetadata, isLoading } = useContractMetadata(contract);
 
-  return <div>ERC1155ProjectPage</div>;
+  return (
+    <div className={styles.container}>
+      <HeroCard
+        isLoading={isLoading}
+        title={contractMetadata?.name!}
+        description={contractMetadata?.description!}
+        image={contractMetadata?.image!}
+      />
+    </div>
+  );
 };
 
 export default ERC1155ProjectPage;
