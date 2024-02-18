8cbf82acbf16076aae17d1b65b7d039c7a4b6b28
Efe G <efeasiughu@gmail.com>
2024-02-18 17:07:04 -0500

    integrated most of my smart contract

--- pages/project/tipjar.tsx
+++ pages/project/tipjar.tsx
@@ -1,5 +1,15 @@
+import styles from "../../styles/Home.module.css";
+import { HeroCard } from "../../components";
+import { TIP_JAR_CONTRACT_ADDRESS } from "../../constants/addresses";
+import { useContract, useContractMetadata } from "@thirdweb-dev/react";
+
 const TipJarProjectPage = () => {
-  return <div>TipJarProjectPage</div>;
+  const { contract } = useContract(TIP_JAR_CONTRACT_ADDRESS);
+  const { data: contractMetadata, isLoading } = useContractMetadata(contract);
+
+  return <div className={styles.container}>
+    
+  </div>;
 };
 
 export default TipJarProjectPage;
