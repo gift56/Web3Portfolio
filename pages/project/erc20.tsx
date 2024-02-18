e0027b4690e4a8f1813ff06dba20ca3276673a9f
Efe G <efeasiughu@gmail.com>
2024-02-18 08:39:11 -0500

    working on some component creation

--- pages/project/erc20.tsx
+++ pages/project/erc20.tsx
@@ -1,7 +1,12 @@
+import { HeroCard } from "../../components";
 import styles from "../../styles/Home.module.css";
 
 const ERC20ProjectPage = () => {
-  return <div className={styles.container}>ERC20ProjectPage</div>;
+  return (
+    <div className={styles.container}>
+      <HeroCard />
+    </div>
+  );
 };
 
 export default ERC20ProjectPage;
