ee6cde42d49037ead79f18ffd7bf0023a3b910d5
Efe G <efeasiughu@gmail.com>
2024-02-17 19:46:20 -0500

    working on some component creation

--- components/index.tsx
+++ components/index.tsx
@@ -1,3 +1,17 @@
+import Navbar from "./Navbar";
+import NftCard from "./NftCard";
+import NftGrid from "./NftGrid";
+import ProfileERC1155 from "./ProfileERC1155";
+import ProfileERC721 from "./ProfileERC721";
+import StakingNftCard from "./StakingNftCard";
+import StakingNftGrid from "./StakingNftGrid";
 
-
-export {};
+export {
+  Navbar,
+  NftCard,
+  NftGrid,
+  ProfileERC1155,
+  ProfileERC721,
+  StakingNftCard,
+  StakingNftGrid,
+};
