34d4396ec8470bdee18476bae4482fe3ca75f0b3
Efe G <efeasiughu@gmail.com>
2024-02-18 17:47:46 -0500

    integrated most of my smart contract

--- pages/project/profilestatus.tsx
+++ pages/project/profilestatus.tsx
@@ -6,7 +6,17 @@ import { useContract, useContractMetadata } from "@thirdweb-dev/react";
 const ProfileStatusPage = () => {
   const { contract } = useContract(PROFILE_STATUS_CONTRACT_ADDRESS);
   const { data: contractMetadata, isLoading } = useContractMetadata(contract);
-  return <div>ProfileStatusPage</div>;
+
+  return (
+    <div className={styles.container}>
+      <HeroCard
+        isLoading={isLoading}
+        title={contractMetadata?.name!}
+        description={contractMetadata?.description!}
+        image={contractMetadata?.image!}
+      />
+    </div>
+  );
 };
 
 export default ProfileStatusPage;
