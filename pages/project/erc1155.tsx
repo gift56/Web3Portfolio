ded7506ed51a54ddc93b7ae0ac3864cd95de8aec
Efe G <efeasiughu@gmail.com>
2024-02-18 16:13:06 -0500

    working on some component creation

--- pages/project/erc1155.tsx
+++ pages/project/erc1155.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useContract, useContractMetadata } from "@thirdweb-dev/react";
+import React from "react";
+import { ERC1155_CONTRACT_ADDRESS } from "../../constants/addresses";
 
 const ERC1155ProjectPage = () => {
-  return (
-    <div>ERC1155ProjectPage</div>
-  )
-}
+  const { contract } = useContract(ERC1155_CONTRACT_ADDRESS, "edition-drop");
+  const { data: contractMetadata, isLoading } = useContractMetadata(contract);
 
-export default ERC1155ProjectPage
+  return <div>ERC1155ProjectPage</div>;
+};
+
+export default ERC1155ProjectPage;
