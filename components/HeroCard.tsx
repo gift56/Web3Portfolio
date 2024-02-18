62effa3adb8491dfd68bd89cbb799be2c606627d
Efe G <efeasiughu@gmail.com>
2024-02-18 08:26:25 -0500

    working on some component creation

---
+++ components/HeroCard.tsx
@@ -0,0 +1,12 @@
+type HeroCardProps = {
+  isLoading: boolean;
+  title: string;
+  description: string;
+  image: string;
+};
+
+const HeroCard = () => {
+  return <div>HeroCard</div>;
+};
+
+export default HeroCard;
