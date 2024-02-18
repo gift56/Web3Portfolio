30dbdabf56be5c98127a23756b6436b8b171054e
Efe G <efeasiughu@gmail.com>
2024-02-18 08:42:31 -0500

    working on some component creation

--- pages/project/erc20.tsx
+++ pages/project/erc20.tsx
@@ -1,7 +1,11 @@
+import { useContract } from "@thirdweb-dev/react";
 import { HeroCard } from "../../components";
 import styles from "../../styles/Home.module.css";
+import { ERC20_CONTRACT_ADDRESS } from "../../constants/addresses";
 
 const ERC20ProjectPage = () => {
+  const { contract } = useContract(ERC20_CONTRACT_ADDRESS, "token");
+
   return (
     <div className={styles.container}>
       <HeroCard isLoading={} title={} description={} image={} />
