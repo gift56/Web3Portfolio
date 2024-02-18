41557d21a2bf662711d31ce6b3ecd4e994fc2954
Efe G <efeasiughu@gmail.com>
2024-02-18 09:52:40 -0500

    working on some component creation

--- pages/project/erc721.tsx
+++ pages/project/erc721.tsx
@@ -1,8 +1,22 @@
+import { useContract, useContractMetadata } from "@thirdweb-dev/react";
+import { ERC721_CONTRACT_ADDRESS } from "../../constants/addresses";
 import styles from "../../styles/Home.module.css";
+import { HeroCard } from "../../components";
 
 const ERC721ProjectPage = () => {
+  const { contract } = useContract(ERC721_CONTRACT_ADDRESS, "nft-drop");
+  const { data: contractMetadata, isLoading } = useContractMetadata(contract);
 
-  return <div className={styles.container}>ERC721ProjectPage</div>;
+  return (
+    <div className={styles.container}>
+      <HeroCard
+        isLoading={isLoading}
+        title={contractMetadata?.name!}
+        description={contractMetadata?.description!}
+        image={contractMetadata?.image!}
+      />
+    </div>
+  );
 };
 
 export default ERC721ProjectPage;
