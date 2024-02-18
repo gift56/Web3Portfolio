ef21a49256e30d2ad8ff9069c63c253316ca9959
Efe G <efeasiughu@gmail.com>
2024-02-18 08:44:38 -0500

    working on some component creation

--- pages/project/erc20.tsx
+++ pages/project/erc20.tsx
@@ -1,14 +1,20 @@
-import { useContract } from "@thirdweb-dev/react";
+import { useContract, useContractMetadata } from "@thirdweb-dev/react";
 import { HeroCard } from "../../components";
 import styles from "../../styles/Home.module.css";
 import { ERC20_CONTRACT_ADDRESS } from "../../constants/addresses";
 
 const ERC20ProjectPage = () => {
   const { contract } = useContract(ERC20_CONTRACT_ADDRESS, "token");
+  const { data: contractMetadata, isLoading } = useContractMetadata(contract);
 
   return (
     <div className={styles.container}>
-      <HeroCard isLoading={} title={} description={} image={} />
+      <HeroCard
+        isLoading={isLoading}
+        title={contractMetadata?.name!}
+        description={contractMetadata?.description!}
+        image={contractMetadata?.image!}
+      />
     </div>
   );
 };
