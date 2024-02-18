7769a6bcaa8ca74224613140445fa2ef77a1006f
Efe G <efeasiughu@gmail.com>
2024-02-18 07:49:04 -0500

    working on some component creation

--- components/ContractCard.tsx
+++ components/ContractCard.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import { useContract, useContractMetadata } from "@thirdweb-dev/react";
+
 type ContractCardProp = {
   href: string;
   contractAddress: string;
@@ -5,8 +9,12 @@ type ContractCardProp = {
   description: string;
 };
 
-const ContractCard = () => {
-  return <div>ContractCard</div>;
+const ContractCard = (props: ContractCardProp) => {
+  const { contract } = useContract(props.contractAddress);
+
+  const { data: contractMetadata, isLoading } = useContractMetadata(contract);
+
+  return <Link href={props.href} className={styles.card}></Link>;
 };
 
 export default ContractCard;
