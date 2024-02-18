e33afaaf236a57f5e1c73cc1ba7f36f56178f605
Efe G <efeasiughu@gmail.com>
2024-02-17 20:09:16 -0500

    working on some component creation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,14 @@
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styles.navbar}>
+      <Link href="/">
+        <p className={styles.gradientText1}>Web3Portfolio</p>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
