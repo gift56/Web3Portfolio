f4ee3199d0f9417817f1fc59bc6275f20cceee9e
Efe G <efeasiughu@gmail.com>
2024-02-18 16:16:44 -0500

    working on some component creation

--- pages/project/erc1155.tsx
+++ pages/project/erc1155.tsx
@@ -1,4 +1,4 @@
-import { useContract, useContractMetadata } from "@thirdweb-dev/react";
+import { Web3Button, useContract, useContractMetadata } from "@thirdweb-dev/react";
 import React from "react";
 import { ERC1155_CONTRACT_ADDRESS } from "../../constants/addresses";
 import { HeroCard } from "../../components";
@@ -16,6 +16,43 @@ const ERC1155ProjectPage = () => {
         description={contractMetadata?.description!}
         image={contractMetadata?.image!}
       />
+      <div className={styles.grid}>
+        <div className={styles.stakeSection}>
+          <h3>Claim ERC1155</h3>
+          <p>Claim an ERC1155 NFT for 10 ERC20 tokens.</p>
+          <Web3Button
+            contractAddress={ERC1155_CONTRACT_ADDRESS}
+            // action={(contract) => contract.erc1155.claim(1)}
+            
+          >
+            Claim NFT
+          </Web3Button>
+        </div>
+        <div className={styles.stakeSection}>
+          <h3>Contract Stats</h3>
+          <p>
+            Total Supply:
+            {isLoadingTotalSupply
+              ? "Loading Total Supply..."
+              : ` ${totalSupply?.toNumber()}`}
+          </p>
+          <p>
+            Total Claim:
+            {isLoadingClaimedTotalSuppy
+              ? "Loading Total Claimed..."
+              : ` ${totalClaimedSupply?.toNumber()}`}
+          </p>
+        </div>
+        <div className={styles.stakeSection}>
+          <h3>Your NFTS</h3>
+          <p>
+            Total Owned:{" "}
+            {isLoadingOwnedNfts
+              ? "Loading Owned Nfts..."
+              : ` ${ownedNfts?.length}`}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
