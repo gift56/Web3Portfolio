73edc76af45d59a7c7befb399c02c43c42820848
Efe G <efeasiughu@gmail.com>
2024-02-18 20:21:03 -0500

    working on last staking contract integration

--- components/StakedNftCard.tsx
+++ components/StakedNftCard.tsx
@@ -1,7 +1,14 @@
 type NftProps = { tokenId: Number };
 
 const StakedNftCard = ({ tokenId }: NftProps) => {
-  return <div>StakedNftCard</div>;
+  return (
+    <div
+      className={styles.card}
+      style={{ width: "100%", paddingBottom: "16px" }}
+    >
+      StakedNftCard
+    </div>
+  );
 };
 
 export default StakedNftCard;
