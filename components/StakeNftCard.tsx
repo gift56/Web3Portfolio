63db7e993a3e33c8fbccfa2677ff4a285104cf56
Efe G <efeasiughu@gmail.com>
2024-02-18 19:59:02 -0500

    working on last staking contract integration

--- components/StakeNftCard.tsx
+++ components/StakeNftCard.tsx
@@ -1,11 +1,21 @@
 import { NFT } from "@thirdweb-dev/sdk";
+import styles from "../styles/Home.module.css";
+import { ThirdwebNftMedia } from "@thirdweb-dev/react";
 
 type NFTProps = {
   nft: NFT;
 };
 
 const StakeNftCard = ({ nft }: NFTProps) => {
-  return <div className={styles.card}>StakeNftCard</div>;
+  return (
+    <div className={styles.card}>
+      <ThirdwebNftMedia metadata={nft.metadata} />
+      <div className={styles.cardText}>
+        <h2 className={styles.gradientText1}>{nft.metadata.name}</h2>
+        <p>Token ID#{nft.metadata.id}</p>
+      </div>
+    </div>
+  );
 };
 
 export default StakeNftCard;
