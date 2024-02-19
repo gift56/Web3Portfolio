514a9a0ddfc91ec727349e31694ba3bfcb98b958
Efe G <efeasiughu@gmail.com>
2024-02-18 19:07:29 -0500

    working on last staking contract integration

--- pages/project/staking.tsx
+++ pages/project/staking.tsx
@@ -1,5 +1,11 @@
-import { useAddress, useContract, useContractMetadata } from "@thirdweb-dev/react";
+import {
+  useAddress,
+  useContract,
+  useContractMetadata,
+} from "@thirdweb-dev/react";
 import { STAKING_CONTRACT_ADDRESS } from "../../constants/addresses";
+import { HeroCard } from "../../components";
+import styles from "../../styles/Home.module.css";
 
 const StakingProjectPage = () => {
   const address = useAddress();
@@ -7,7 +13,16 @@ const StakingProjectPage = () => {
   const { contract } = useContract(STAKING_CONTRACT_ADDRESS);
   const { data: contractMetadata, isLoading } = useContractMetadata(contract);
 
-  return <div>StakingProjectPage</div>;
+  return (
+    <div className={styles.container}>
+      <HeroCard
+        isLoading={isLoading}
+        title={contractMetadata?.name!}
+        description={contractMetadata?.description!}
+        image={contractMetadata?.image!}
+      />
+    </div>
+  );
 };
 
 export default StakingProjectPage;
