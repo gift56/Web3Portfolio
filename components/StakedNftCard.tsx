7d6ef42891bcd18257a8c5f17ac3b76370198c54
Efe G <efeasiughu@gmail.com>
2024-02-18 20:24:01 -0500

    working on last staking contract integration

--- components/StakedNftCard.tsx
+++ components/StakedNftCard.tsx
@@ -1,8 +1,22 @@
+import { useContract, useNFT } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
+import {
+  ERC721_CONTRACT_ADDRESS,
+  STAKING_CONTRACT_ADDRESS,
+} from "../constants/addresses";
 
-type NftProps = { tokenId: Number };
+type NftProps = { tokenId: number };
 
 const StakedNftCard = ({ tokenId }: NftProps) => {
+  const { contract: ERC721Contract } = useContract(
+    ERC721_CONTRACT_ADDRESS,
+    "nft-drop"
+  );
+
+  const { contract: StakingContract } = useContract(STAKING_CONTRACT_ADDRESS);
+
+  const {} = useNFT(ERC721Contract, tokenId);
+
   return (
     <div
       className={styles.card}
