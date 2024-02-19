d9760aecef76ed17206699de05278406c6f74d54
Efe G <efeasiughu@gmail.com>
2024-02-18 20:41:04 -0500

    Hoorray just completed my journey as a web3 dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { ContractCard } from "../components";
 import {
   ERC1155_CONTRACT_ADDRESS,
@@ -12,67 +13,72 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            My{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/dashboard/contracts/deploy"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Contracts
-              </a>
-            </span>
-          </h1>
+    <>
+      <Head>
+        <title></title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <h1 className={styles.title}>
+              My{" "}
+              <span className={styles.gradientText0}>
+                <a
+                  href="https://thirdweb.com/dashboard/contracts/deploy"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Contracts
+                </a>
+              </span>
+            </h1>
 
-          <p className={styles.description}>
-            Select a contract to interact with.
-          </p>
-        </div>
+            <p className={styles.description}>
+              Select a contract to interact with.
+            </p>
+          </div>
 
-        <div className={styles.grid}>
-          <ContractCard
-            href="/project/erc20"
-            contractAddress={ERC20_CONTRACT_ADDRESS}
-            title="ERC20 →"
-            description="Claim ERC20 Token"
-          />
-          <ContractCard
-            href="/project/erc721"
-            contractAddress={ERC721_CONTRACT_ADDRESS}
-            title="ERC721 →"
-            description="Claim ERC721 Token"
-          />
-          <ContractCard
-            href="/project/erc1155"
-            contractAddress={ERC1155_CONTRACT_ADDRESS}
-            title="ERC1155 →"
-            description="Claim ERC1155 Token"
-          />
-          <ContractCard
-            href="/project/staking"
-            contractAddress={STAKING_CONTRACT_ADDRESS}
-            title="Staking →"
-            description="Stake your ERC721 NFT to earn ERC20 token."
-          />
-          <ContractCard
-            href="/project/profilestatus"
-            contractAddress={PROFILE_STATUS_CONTRACT_ADDRESS}
-            title="Profile Status →"
-            description="Update your profile status on the blockchain"
-          />
-          <ContractCard
-            href="/project/tipjar"
-            contractAddress={TIP_JAR_CONTRACT_ADDRESS}
-            title="Tip Jar →"
-            description="Feel free to leave a tip on the tip jar."
-          />
+          <div className={styles.grid}>
+            <ContractCard
+              href="/project/erc20"
+              contractAddress={ERC20_CONTRACT_ADDRESS}
+              title="ERC20 →"
+              description="Claim ERC20 Token"
+            />
+            <ContractCard
+              href="/project/erc721"
+              contractAddress={ERC721_CONTRACT_ADDRESS}
+              title="ERC721 →"
+              description="Claim ERC721 Token"
+            />
+            <ContractCard
+              href="/project/erc1155"
+              contractAddress={ERC1155_CONTRACT_ADDRESS}
+              title="ERC1155 →"
+              description="Claim ERC1155 Token"
+            />
+            <ContractCard
+              href="/project/staking"
+              contractAddress={STAKING_CONTRACT_ADDRESS}
+              title="Staking →"
+              description="Stake your ERC721 NFT to earn ERC20 token."
+            />
+            <ContractCard
+              href="/project/profilestatus"
+              contractAddress={PROFILE_STATUS_CONTRACT_ADDRESS}
+              title="Profile Status →"
+              description="Update your profile status on the blockchain"
+            />
+            <ContractCard
+              href="/project/tipjar"
+              contractAddress={TIP_JAR_CONTRACT_ADDRESS}
+              title="Tip Jar →"
+              description="Feel free to leave a tip on the tip jar."
+            />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
